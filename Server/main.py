6dcd7eca4dd180484807fb3d5a44d3898c86c5f6
AlexNay1383 <alexnay1383@gmail.com>
2024-02-15 21:31:37 +0200

    Added temporary socket connection at local host

--- Server/main.py
+++ Server/main.py
@@ -4,6 +4,7 @@ import json
 HOST = None
 PORT = None
 MAX_CLIENTS = None
+MAX_REQ_SIZE = None
 
 #Read init values from ./values.json
 with open("values.json", "r") as json_stream:
@@ -11,3 +12,14 @@ with open("values.json", "r") as json_stream:
     MAX_CLIENTS = val["MAX_CLIENTS"]
     HOST = val["HOST"]
     PORT = val["PORT"]
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(MAX_CLIENTS)
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by: {addr}")
+        while True:
+            req = conn.recv(1024)
+            if not req: break
+            conn.sendall(req)
